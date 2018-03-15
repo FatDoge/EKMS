7a16e49c953e5cfd7a3d4dd070c36275d1603e20
fatdoge <admin@fatdoge.cn>
2018-03-15 21:32:38 +0800

    fix bugs

---
+++ js/login.js
@@ -0,0 +1,56 @@
+//api url
+var url='http://192.168.43.198';
+function login(){
+    console.log('login...');
+    var email=$("#email").val();
+    var password=$("#password").val();
+    var token=$.md5(email+password);
+    console.log('token:',token);
+    loginCode(email,token);
+}
+function loginCode(email,token){
+    var newToken;
+    $.ajax({
+        url:url+'/login_init',
+        success:function(rel){
+            if(rel.code===200)
+            console.log(rel);
+            newToken=token+rel.data.token;
+            console.log(email,newToken)
+            ajaxLoginInfo(email,newToken);
+        },
+        error:function(err){
+            console.log(err)
+        },
+        xhrFields: {
+            withCredentials: true
+         },
+         crossDomain: true,
+     
+        
+    })
+    return newToken;
+}
+function ajaxLoginInfo(username,password){
+    $.ajax({
+        url:url+'/login_confirm',
+        data:{
+            username:username,
+            password:password
+        },
+        success:function(rel){
+            console.log(rel);
+        },
+        error:function(err){
+            console.log(err);
+        },
+        xhrFields: {
+            withCredentials: true
+         },
+         crossDomain: true,
+     
+         
+     
+        
+    })
+}
