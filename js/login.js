c74d1365b49cd925ca829044ad79619609368c72
fatdoge <admin@fatdoge.cn>
2018-03-16 20:13:21 +0800

    change to use webstorm

--- js/login.js
+++ js/login.js
@@ -1,12 +1,35 @@
 //api url
 var url='http://192.168.2.133';
+function isEmail(str){
+    var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+    console.log(reg.test(str))
+    return reg.test(str);
+}
+function msg(msg){
+    layui.use('layer',function(){
+        var layer=layui.layer;
+        layer.msg(msg,{
+            offset:['50%','auto'],
+            shift: 6
+        });
+    })
+}
+function jumpUrl(roll){
+    //roll 1:admin 0:user
+    roll?window.location='./adminIndex.html':window.location='#user'
+}
 function login(){
     console.log('login...');
     var email=$("#email").val();
     var password=$("#password").val();
-    var token=$.md5(email+password);
-    console.log('token:',token);
-    loginCode(email,token);
+    if(isEmail(email)){
+        var token=$.md5(email+password);
+        console.log('token:',token);
+        loginCode(email,token);
+    }else{
+        msg('用户名格式错误!');
+    }
+
 }
 function loginCode(email,token){
     var newToken;
@@ -40,7 +63,9 @@ function ajaxLoginInfo(username,password){
             password:password
         },
         success:function(rel){
-            console.log(rel);
+            if(rel.code===200)
+            jumpUrl(rel.data.role);
+            else msg('登陆失败');
         },
         error:function(err){
             console.log(err);
