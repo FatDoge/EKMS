5df74b5e8bca82849a3077b5a96fd273c636d09f
fatdoge <admin@fatdoge.cn>
2018-03-27 22:52:11 +0800

    :star:整合普通用户上传知识页面

---
+++ js/userOperations.js
@@ -0,0 +1,28 @@
+
+//普通用户收藏知识
+function collectKnowledge(){
+console.log("用户收藏知识");
+    layui.use('layer',function(){
+        var layer=layui.layer;
+        layer.msg('收藏成功',{
+            area:['150px',''],
+            offset:['40%','40%'],
+        });
+    })
+}
+//普通用户上传知识
+function openFileUploadPage(){
+    //iframe层-禁滚动条
+    layui.use('layer',function(){
+        var layer=layui.layer;
+        //iframe层-禁滚动条
+        var index = layer.open({
+            type: 2,
+            content: './userFileUpload.html',
+            title: '知识上传',
+            area:['400px','150px;'],
+            maxmin: false
+        });
+        layer.full(index);
+    })
+}
